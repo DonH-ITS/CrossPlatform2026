a1e63acfeb585a2b780ceca854a8a0e80aacdfce
Donny <32511652+DonH-ITS@users.noreply.github.com>
2026-09-14 21:37:58 +0100

    update lecture notes

---
+++ Week2/LectureSamples/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp10
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage() {
+            InitializeComponent();
+
+            GreetingLabel.Text = "...but this text was set in the code-behind!";
+
+            // 2. Dynamic UI
+            for (int i = 1; i <= 5; i++) {
+                Label label = new Label
+                {
+                    Text = $"Dynamically created item {i}"
+                };
+
+                DynamicItemsLayout.Add(label);
+
+            }
+        }
+    }
+}
